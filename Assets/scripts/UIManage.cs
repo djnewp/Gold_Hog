51b22b4b088a09a445329c7004350601e551a14f
djnewp <hjyv1234@gmail.com>
2020-08-03 12:16:13 +0900

    asdf

--- Assets/scripts/UIManage.cs
+++ Assets/scripts/UIManage.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class UIManage : MonoBehaviour
 {
+    GameObject _guy;
+    Rigidbody2D _rigbd;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _guy = GameObject.Find("guy");
+        _rigbd = _guy.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
