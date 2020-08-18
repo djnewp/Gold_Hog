1319974f7ae6231ee8d5482beb3792df3f878a31
djnewp <hjyv1234@gmail.com>
2020-08-18 10:20:37 +0900

    인스펙터 버튼 추가하기

---
+++ Assets/scripts/Editor/GuyInspector.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CanEditMultipleObjects]
+[CustomEditor(typeof(mainchar))]
+
+public class GuyInspector : Editor
+{
+    mainchar _target;
+
+    public override void OnInspectorGUI()
+    {
+        if (_target == null) _target = target as mainchar;
+
+        if (GUILayout.Button("데미지")) _target.OnDmg(1000);
+
+        base.OnInspectorGUI();
+
+    }
+}
