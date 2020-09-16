c9f9cbbf8fb3ed2e526c94de6eaae6a8dddfd002
djnewp <hjyv1234@gmail.com>
2020-09-16 15:29:43 +0900

    자동생성 스크립트 작성 중

--- Assets/scripts/ObstacleGenerator.cs
+++ Assets/scripts/ObstacleGenerator.cs
@@ -6,11 +6,44 @@ using System;
 public class ObstacleGenerator : MonoBehaviour
 {
     public GameObject[] _TerrainPrefab;
-    public List<obstacle> _PrefabedTerrain;
+    
 
-    // Update is called once per frame
-    void Update()
+    public void Init()
     {
         
     }
+    // Update is called once per frame
+    void Update()
+    {
+          
+        
+
+    }
+
+    private void NewLand()
+    {
+        if (_TerrainPrefab != null)
+        {
+            Transform parent = GameObject.Find("map").GetComponent<Transform>();
+            GameObject child = Instantiate(_TerrainPrefab[0]);
+            child.transform.parent = parent;
+            child.transform.position = new Vector3(child.transform.position.x,
+                                                   gameObject.transform.position.y
+                                                    , child.transform.position.z);
+            child.name = "Blocks";
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+ 
+            
+            NewLand();
+       
+    }
+
+    //1. 지형이 특정 지점을 통과하면
+    //2. 새로운 지형 생성
+    //3. 그러는 도중 생성된 지형이 특정 지점을 통과하면
+    //4. 지형 파괴
 }
--- Assets/scripts/actor/Turrain.cs
+++ Assets/scripts/actor/Turrain.cs
@@ -5,4 +5,6 @@ using UnityEngine;
 public abstract class Turrain : MonoBehaviour
 {
     protected abstract void Gone();
+
+    
 }
